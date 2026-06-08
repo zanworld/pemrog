0ad3e57b4204bfee4b4339f7a2188b227b8c6ffa
zanworld <famzzmakruf@gmail.com>
2026-06-08 13:51:07 +0700

    fix: change base to relative path in vite.config.js for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/pemrog/',
+  base: './',
   plugins: [react()],
   server: {
     proxy: {
